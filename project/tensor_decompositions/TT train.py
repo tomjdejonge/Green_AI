b291b84f3db56c04ec8c3a8057dc239cb37bf4c2
0578Tex <104349767+0578Tex@users.noreply.github.com>
2022-05-02 10:37:46 +0200

    TTtrain

--- project/tensor_decompositions/TT train.py
+++ project/tensor_decompositions/TT train.py
@@ -1,5 +1,12 @@
 import matplotlib.image as image
+import numpy as np
 img=image.imread('dog.jpg')
 print('The Shape of the image is:',img.shape)
 print('The image as array is:')
 print(img)
+reshape = np.reshape (img, (512,1536))
+svd = np.linalg.svd(reshape,compute_uv=True)
+sv, u, d = svd
+print(f'sv = {sv.shape}')
+print(f'u = {u.shape}')
+print(f'd = {d.shape}')
