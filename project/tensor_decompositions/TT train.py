82397ca5fe16404c6a8f507ccae026396311b058
0578Tex <104349767+0578Tex@users.noreply.github.com>
2022-05-02 10:46:21 +0200

    TTtrain

--- project/tensor_decompositions/TT train.py
+++ project/tensor_decompositions/TT train.py
@@ -6,7 +6,7 @@ print('The image as array is:')
 print(img)
 reshape = np.reshape (img, (512,1536))
 svd = np.linalg.svd(reshape,compute_uv=True)
-sv, u, d = svd
-print(f'sv = {sv.shape}')
+u, s, vh = svd
+print(f'vh = {vh.shape}')
 print(f'u = {u.shape}')
 print(f'd = {d.shape}')
