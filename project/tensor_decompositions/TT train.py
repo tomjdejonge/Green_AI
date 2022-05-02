9704deecb5bf0baf0ea7536a74680d825a4da8b1
0578Tex <104349767+0578Tex@users.noreply.github.com>
2022-05-02 15:06:32 +0200

    TTtrain

--- project/tensor_decompositions/TT train.py
+++ project/tensor_decompositions/TT train.py
@@ -1,13 +1,44 @@
 import matplotlib.image as image
 import numpy as np
+from skimage import color
+from skimage import io
+from PIL import Image
+import matplotlib.pyplot as plt
+
 img=image.imread('dog.jpg')
-print('The Shape of the image is:',img.shape)
-print('The image as array is:')
-print(img)
-reshape = np.reshape (img, (512,1536))
-svd = np.linalg.svd(reshape,compute_uv=True)
-u, s, vh = svd
-print(f'u = {u.shape}')
-print(f's = {s.shape}')
-print(f'vh = {vh.shape}')
-print(s)
+dog = img.reshape(4,4,4,4,4,4,4,4,4,3)
+print(dog.shape)
+
+def svd(pic):
+    svd = np.linalg.svd(pic, full_matrices=True, compute_uv=True)
+    u, s, vh = svd
+    return u, s, vh
+
+def TT(pic):
+    # variables
+    ut = []
+    st = []
+    vht = []
+    # calculate delta
+    #delta = (eps/(np.sqrt(d-1))) * pic.length()
+
+    #iterate
+    for i in range(8):
+        u, s, vh = svd(pic)
+        ut.append(u)
+        st.append(s)
+        vht.append(vh)
+        pic = s
+        print(s.shape)
+    #print(st)
+
+    return None
+
+print(f'hoi, {TT(dog)}')
+
+
+
+
+
+
+
