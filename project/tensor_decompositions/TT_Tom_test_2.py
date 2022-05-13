0ce0b23dcb77be04ee4e30fe89f8b9ab69c311ed
tomjdejonge <tomjacobdejonge@gmail.com>
2022-05-13 16:54:42 +0200

    Evas comments

--- project/tensor_decompositions/TT_Tom_test_2.py
+++ project/tensor_decompositions/TT_Tom_test_2.py
@@ -2,11 +2,12 @@ from PIL import Image
 from scipy import linalg
 import numpy as np
 import tensorly as ty
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
-img = Image.open('dog.jpg')
+img = Image.open('dog.jpg_small.jpeg')
 p = np.asarray(img)
+# print(p.shape)
 a = p
 
 d = 3
@@ -38,10 +39,10 @@ for k in range(d-1):
     s = np.reshape(s, (s.shape[0], 1))
     # print(s.shape)
     # print(s)
-    # print('s = ', s.shape)
-    # print('U = ', U.shape)
-    # print('S = ', S.shape)
-    # print('V = ', V.shape)
+    # print('s = ', s)
+    # print('U = ', U)
+    # print('S = ', S)
+    # print('V = ', V)
 
     rank = 0
     # print(S[rank])
@@ -55,25 +56,25 @@ for k in range(d-1):
     # print(int(n[k]))
     # print(int(r[k]))
     # print((U[:, :int(r[k+1])]).shape)
-    g.append(np.reshape(U[:, :int(r[k+1])], [int(r[k]), int(n[k]), int(r[k+1])], order="F"))
+    g.append(np.reshape(U[:, :int(r[k+1])], [int(r[k]), int(n[k]), int(r[k+1])]))
     x = np.reshape(U[:, :int(r[k+1])], [int(r[k]), int(n[k]), int(r[k+1])], order="F")
-    print(x)
-    print(x.shape)
+    # print(x)
+    # print(x.shape)
     # print(np.linalg.norm(g[k]))
     # print(S.shape)
     p_1 = S[:int(r[k + 1]), :int(r[k + 1])]
-    # print(p_1.shape)
+    # print(p_1)
     p_2 = V[:, :int(r[k + 1])]
-    # print(p_2.shape)
+    # print(p_2)
     c = p_1 @ p_2.transpose()
     # print(c)
 # print(r[d-1])
 # print(n[d-1])
 # print(r[d])
 g.append(np.reshape(c, (int(r[d-1]), int(n[d-1]), int(r[d])), order="F"))
-# print(g[0].shape)
-# print(g[1].shape)
-# print(g[2].shape)
+print(g[0].transpose())
+print(g[1])
+print(g[2])
 g1 = np.transpose(g[0], [1, 2, 0])
 g1 = np.squeeze(g1)
 g2 = g[1]
@@ -97,3 +98,15 @@ I = ty.tenalg.mode_dot(g2, g3, 2)
 # B = np.squeeze(B)
 # B = np.transpose(B)
 # print(B.shape)
+
+def compare(image1, image2):
+    f = plt.figure()
+    f.add_subplot(1,2, 1)
+    plt.imshow(image1)
+    plt.axis('off')
+    f.add_subplot(1,2,2)
+    plt.imshow(image2)
+    plt.axis('off')
+    plt.show(block=True)
+
+# compare(img, img)
