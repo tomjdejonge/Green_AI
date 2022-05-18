8f70732cea3554580c26ccea7db31c3b90ce2603
0578Tex <104349767+0578Tex@users.noreply.github.com>
2022-05-18 11:25:49 +0200

    opschonen

--- project/tensor_decompositions/N-Mode.py
+++ project/tensor_decompositions/N-Mode.py
@@ -11,6 +11,12 @@ core, d, r, n = tensortrain(img)
 B = tt_reconstruction(core, d, r, n)
 # print(B.shape)
 
+A = np.array([[1,2,3],[4,5,6]])
+Y = np.array([[[1,4,7,10], [13,16,19,22]],
+ [[2,5,8,11], [14,17,20,23]],
+ [[3,6,9,12], [15,18,21,24]]])
+
+# print(Y.shape)
 
 def unfold(tensor, x):
 
@@ -18,20 +24,25 @@ def unfold(tensor, x):
         shape = tensor.shape
         res = np.reshape(tensor, (shape[0],shape[1]*shape[2]))
     elif x==2:
-        tensor = tensor.transpose(1,0,2)
+        tensor = tensor.transpose(2,1,0)
         shape = tensor.shape
         res = np.reshape(tensor, (shape[0],shape[1]*shape[2]))
     elif x==3:
-        tensor = tensor.transpose(2, 0, 1)
+        tensor = tensor.transpose(1, 2, 0)
         shape = tensor.shape
         res = np.reshape(tensor, (shape[0], shape[1] * shape[2]))
     return res
 
+# print(unfold(Y,1).shape)
+# print(unfold(Y,2).shape)
+# print(unfold(Y,3).shape)
+
 #nmodeproduct
-A = np.array([[1,2,3],[4,5,6]])
-Y = np.array([[[1,4,7,10], [13,16,19,22]],
- [[2,5,8,11], [14,17,20,23]],
- [[3,6,9,12], [15,18,21,24]]])
 
-print(unfold(Y,1).dot(A))
-
+Y = unfold(Y,1)
+print(Y@A)
+# print(np.matmul(unfold(Y,1),(A)))
+# print(Y.shape, A.shape)
+# print(unfold(Y,1).shape)
+#
+# print(unfold(Y,1).dot(A))
