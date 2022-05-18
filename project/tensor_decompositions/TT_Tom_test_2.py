8aefa390497d52b78a05573832f4c397d4aa11cb
tomjdejonge <tomjacobdejonge@gmail.com>
2022-05-18 13:57:37 +0200

    changes to TT_Tom_test_2.py

--- project/tensor_decompositions/TT_Tom_test_2.py
+++ project/tensor_decompositions/TT_Tom_test_2.py
@@ -6,75 +6,68 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    original_image = Image.open('dog.jpg_small.jpeg')
-    tt, d = tt_decomposition('dog.jpg_small.jpeg')
-    reconstructed_image = reconstruction(tt, d)
+    original_image = Image.open('dog.jpg')
+    tt, d, r, n = tt_decomposition(original_image)
+    reconstructed_image = reconstruction(tt, d, r, n)
     compare(original_image, reconstructed_image)
 
 
-def tt_decomposition(tensor, epsilon=0.2):
-    img = Image.open(tensor)
-    p = np.asarray(img)
-    a = p
-    d = len(p.shape)
-    r0 = 1
-    r3 = r0
-    delta = (epsilon / np.sqrt(d - 1)) * np.linalg.norm(a)
-    n = a.shape
+def tt_decomposition(tensor, epsilon=0.01):
+    p = np.asarray(np.reshape(tensor, (4, 4, 4, 4, 4, 4, 4, 4, 4, 3)))
+    n = p.shape
+    d = len(n)
+    delta = (epsilon / np.sqrt(d - 1)) * np.linalg.norm(p)
     r = np.zeros(d + 1)
-    r[0] = r0
-    r[d] = r3
+    r[0] = 1
+    r[-1] = 1
     g = []
-    c = a
+    c = p.copy()
     for k in range(d - 1):
+        # print(k)
         m = int(r[k] * n[k])  # r_(k-1)*n_k
         b = int(c.size / m)  # numel(C)/r_(k-1)*n_k
-        c = np.reshape(c, [m, b], order="F")
+        c = np.reshape(c, [m, b])
         [U, S, V] = linalg.svd(c, full_matrices=False)
         V = V.transpose()
         S = np.diag(S)
         s = np.diagonal(S)
         s = np.reshape(s, (s.shape[0], 1))
+        # print(c.shape)
         rank = 0
-        error = np.linalg.norm(s[rank])
+        error = np.linalg.norm(s[rank + 1])
         while error > delta:
             rank += 1
             error = np.linalg.norm(s[rank + 1:])
         r[k + 1] = rank + 1
+        # print(k,r)
         g.append(np.reshape(U[:, :int(r[k + 1])], [int(r[k]), int(n[k]), int(r[k + 1])]))
         p_1 = S[:int(r[k + 1]), :int(r[k + 1])]
         p_2 = V[:, :int(r[k + 1])]
         c = p_1 @ p_2.transpose()
-    g.append(np.reshape(c, (int(r[d - 1]), int(n[d - 1]), int(r[d])), order="F"))
-    return g, d
+    g.append(np.reshape(c, (int(r[d - 1]), int(n[d - 1]), int(r[d]))))
+    print(f'norm of last core =   {linalg.norm(g[-1])}')
+    print(f'norm of tensor =      {linalg.norm(p)}')
+    return g, d, r, n
 
 
-def reconstruction(g, d):
-    g1 = np.transpose(g[0], [1, 2, 0])
-    g1 = np.squeeze(g1)
-    g2 = g[1]
-    g3 = np.transpose(g[d - 1])
-    g3 = np.squeeze(g3)
-    I = ty.tenalg.mode_dot(g2, g3, 2)
-    # print(I)
-    I = rearrange(I)
-    # print(I)
-    B = []
-    for i in range(0, 3):
-        res = np.matmul(g1, (I[:, :, i]))
-        B.append(res)
-    B = np.array(B)
-    B = np.squeeze(B)
-    B = np.transpose(B)
-    c = [[], [], []]
-    for i in range(3):
-        c[i] = (np.transpose(B[:, :, i]))
-    c = np.array(c)
-    Dog = (c.astype(np.uint8))
-    # print(Dog)
-    Dog = rearrange_2(Dog)
-    reconstructed = Image.fromarray(Dog.astype(np.uint8))
-    return reconstructed
+def reconstruction(g, d, r, n):
+    r = list(r.astype(np.uint))
+    # for i in range(len(r)):
+    #     r[i] = int(r[i])
+    # print(f'r= {r}')
+    n = list(n)
+    full_tensor = np.reshape(g[0], (int(n[0]), int(r[1])))
+
+    for k in range(1, d):
+        full_tensor = full_tensor.dot(g[k].reshape(int(r[k]), int(n[k]) * int(r[k + 1])))
+        full_tensor = full_tensor.reshape(np.prod(n[:k + 1]), int(r[k + 1]))
+    # print(full_tensor.shape)
+    q = [2 * i for i in range(d // 2)] + [1 + 2 * i for i in range(d // 2)]
+    # print(f'q = {q}')
+    # full_tensor = full_tensor.reshape(n).transpose(q)
+    # full_tensor = full_tensor * 255 / (abs(full_tensor.min()) + full_tensor.max())
+    z = np.array(np.reshape(full_tensor, (512, 512, 3)))
+    return Image.fromarray((z).astype(np.uint8), 'RGB')
 
 
 def compare(image1, image2):
