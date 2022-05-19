2a25e135b754a86c1126c2ca6d75365dac0b01cf
0578Tex <104349767+0578Tex@users.noreply.github.com>
2022-05-19 15:35:12 +0200

    voor patrick

--- project/tensor_decompositions/N_Mode.py
+++ project/tensor_decompositions/N_Mode.py
@@ -45,21 +45,75 @@ def nmultiplication(tensor, matrix, n):
     res = matrix.dot(unfold(tensor,n))
     return np.reshape(res,(mshape[0],tshape[0],tshape[2]))
 
-X = A.dot(unfold(Y,1))
-# print(np.reshape(X,(2,2,4)).shape)
+
+def unfold(tensor, mode):
+    """Returns the mode-`mode` unfolding of `tensor` with modes starting at `0`.
+
+    Parameters
+    ----------
+    tensor : ndarray
+    mode : int, default is 0
+           indexing starts at 0, therefore mode is in ``range(0, tensor.ndim)``
+
+    Returns
+    -------
+    ndarray
+        unfolded_tensor of shape ``(tensor.shape[mode], -1)``
+    """
+    return ty.reshape(ty.moveaxis(tensor, mode, 0), (tensor.shape[mode], -1))
 
 
-# W = V.dot(unfold(Y,2))
-# print(np.reshape(W,(3,2)))
-print(nmultiplication(np.array(B).reshape(512,3,512),A,1))
-# #nmodeproduct
-# print(unfold(Y,1).dot(A.transpose(1,0)))
-# print(ty.tenalg.mode_dot(Y,A,1))
-# print(ty.tenalg.mode_dot(Y,A,1))
-#
-#
-# print(np.matmul(unfold(Y,1),(A)))
-# print(Y.shape, A.shape)
-# print(unfold(Y,1).shape)
-#
-# print(unfold(Y,1).dot(A))
+def fold(unfolded_tensor, mode, shape):
+
+    full_shape = list(shape)
+    mode_dim = full_shape.pop(mode)
+    full_shape.insert(0, mode_dim)
+    return ty.moveaxis(ty.reshape(unfolded_tensor, full_shape), 0, mode)
+
+def mode_dot(tensor, matrix_or_vector, mode, transpose=False):
+
+    # the mode along which to fold might decrease if we take product with a vector
+    fold_mode = mode
+    new_shape = list(tensor.shape)
+
+    if len(matrix_or_vector.shape) == 2:  # Tensor times matrix
+        # Test for the validity of the operation
+        dim = 0 if transpose else 1
+        if matrix_or_vector.shape[dim] != tensor.shape[mode]:
+            raise ValueError(
+                'shapes {0} and {1} not aligned in mode-{2} multiplication: {3} (mode {2}) != {4} (dim 1 of matrix)'.format(
+                    tensor.shape, matrix_or_vector.shape, mode, tensor.shape[mode], matrix_or_vector.shape[dim]
+                ))
+
+        if transpose:
+            matrix_or_vector = T.conj(T.transpose(matrix_or_vector))
+
+        new_shape[mode] = matrix_or_vector.shape[0]
+        vec = False
+
+    elif T.ndim(matrix_or_vector) == 1:  # Tensor times vector
+        if matrix_or_vector.shape[0] != tensor.shape[mode]:
+            raise ValueError(
+                'shapes {0} and {1} not aligned for mode-{2} multiplication: {3} (mode {2}) != {4} (vector size)'.format(
+                    tensor.shape, matrix_or_vector.shape, mode, tensor.shape[mode], matrix_or_vector.shape[0]
+                ))
+        if len(new_shape) > 1:
+            new_shape.pop(mode)
+        else:
+            # Ideally this should be (), i.e. order-0 tensors
+            # MXNet currently doesn't support this though..
+            new_shape = []
+        vec = True
+
+    else:
+        raise ValueError('Can only take n_mode_product with a vector or a matrix.'
+                         'Provided array of dimension {} not in [1, 2].'.format(T.ndim(matrix_or_vector)))
+
+    res = np.dot(matrix_or_vector, unfold(tensor, mode))
+
+    if vec:  # We contracted with a vector, leading to a vector
+        return ty.reshape(res, shape=new_shape)
+    else:  # tensor times vec: refold the unfolding
+        return fold(res, fold_mode, new_shape)
+
+print(mode_dot(Y, A, 1, transpose=False))
