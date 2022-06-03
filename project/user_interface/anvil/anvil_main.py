8397cb57caf1682a89dd9cfb6fe32153cd45d4a3
tomjdejonge <tomjacobdejonge@gmail.com>
2022-06-03 15:37:20 +0200

    Working TT_SVD app

--- project/user_interface/anvil/anvil_main.py
+++ project/user_interface/anvil/anvil_main.py
@@ -2,23 +2,29 @@ import anvil.server
 import anvil.media
 import io
 from PIL import Image
+from TT_SVD_1 import tensortrain, tt_reconstruction, border
+import numpy as np
 
-
-# dog: "C:\Users\tommo\Downloads\dog.jpg.jpeg"
 
 anvil.server.connect('DH4R5MH3DTKYNYBPF63XKZLV-UHSXWMNTP7IBV2RQ')
 
 @anvil.server.callable
-def get_image_path(file):
-    img = file
-    return img
+def process_image(image):
+    image_object = Image.open(io.BytesIO(image.get_bytes()))
+    image = np.asarray(image_object)
+    return image
 
-# @anvil.server.callable
-# def image_tensor_train(img, epsilon):
-#     g, d, r, n = tensortrain(img, epsilon)
-#     reconstructed = tt_reconstruction(g, d, r, n)
-#     final_image = border(np.array(reconstructed), 0, 255, p=True)
-#     return g, d, r, n, final_image
+@anvil.server.callable
+def image_tensor_train(img, epsilon=0.1):
+    g, d, r, n = tensortrain(img, epsilon)
+    reconstructed = tt_reconstruction(g, d, r, n)
+    new_image = border(np.array(reconstructed), 0, 255, p=True)
+    final_image = Image.fromarray(new_image.astype(np.uint8), 'RGB')
+    bs = io.BytesIO()
+    name = 'final_image'
+    final_image.save(bs, format="JPEG")
+    final = anvil.BlobMedia("image/jpeg", bs.getvalue(), name=name)
+    return g, d, r, n, final
 
 
 anvil.server.wait_forever()
