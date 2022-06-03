9a32c048bf74e6ee74bb1a989869b661f49dd718
tomjdejonge <tomjacobdejonge@gmail.com>
2022-06-03 21:08:45 +0200

    Working TT_SVD app

--- project/user_interface/anvil/anvil_main.py
+++ project/user_interface/anvil/anvil_main.py
@@ -4,15 +4,19 @@ import io
 from PIL import Image
 from TT_SVD_1 import tensortrain, tt_reconstruction, border
 import numpy as np
-
+import pandas as pd
+import anvil.tables as tables
+from anvil.tables import app_tables
 
 anvil.server.connect('DH4R5MH3DTKYNYBPF63XKZLV-UHSXWMNTP7IBV2RQ')
+
 
 @anvil.server.callable
 def process_image(image):
     image_object = Image.open(io.BytesIO(image.get_bytes()))
     image = np.asarray(image_object)
     return image
+
 
 @anvil.server.callable
 def image_tensor_train(img, epsilon=0.1):
@@ -25,6 +29,23 @@ def image_tensor_train(img, epsilon=0.1):
     final_image.save(bs, format="JPEG")
     final = anvil.BlobMedia("image/jpeg", bs.getvalue(), name=name)
     return g, d, r, n, final
+
+
+@anvil.server.callable
+def import_csv_data(file):
+    with anvil.media.TempFile(file) as f:
+        df = pd.read_csv(f)
+        df = df.dropna()
+        for d in df.to_dict(orient="records"):
+            # d is now a dict of {columnname -> value} for this row
+            # We use Python's **kwargs syntax to pass the whole dict as
+            # keyword arguments
+            app_tables.table_0.add_row(**d)
+
+@anvil.server.callable
+def clear_table():
+    app_tables.table_0.delete_all_rows()
+
 
 
 anvil.server.wait_forever()
